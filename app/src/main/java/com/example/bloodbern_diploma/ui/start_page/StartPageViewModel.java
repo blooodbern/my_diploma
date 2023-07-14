043ac65fa0f121ebcdbf891505520dd139c54a83
blooodbern <6906312@stud.nau.edu.ua>
2023-07-14 09:43:58 +0200

    2. add a localization file; create empty StartPageFragmet

---
+++ app/src/main/java/com/example/bloodbern_diploma/ui/start_page/StartPageViewModel.java
@@ -0,0 +1,8 @@
+package com.example.bloodbern_diploma.ui.start_page;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class StartPageViewModel extends ViewModel {
+}
