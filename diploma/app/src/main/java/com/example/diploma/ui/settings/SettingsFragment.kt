a8871ab31e65f0adadbb0d2023917471e5c77d51
blooodbern <6906312@stud.nau.edu.ua>
2023-07-19 18:17:58 +0200

    4. create SettingsFragment

---
+++ diploma/app/src/main/java/com/example/diploma/ui/settings/SettingsFragment.kt
@@ -0,0 +1,42 @@
+package com.example.diploma.ui.settings
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+import com.example.diploma.R
+import com.example.diploma.databinding.FragmentHomeBinding
+import com.example.diploma.databinding.FragmentSettingsBinding
+import com.example.diploma.ui.home.HomeViewModel
+
+
+class SettingsFragment : Fragment() {
+    private var _binding: FragmentSettingsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val settingsViewModel =
+            ViewModelProvider(this).get(SettingsViewModel::class.java)
+
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textSettings
+        settingsViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+    
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
